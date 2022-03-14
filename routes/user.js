cc2b0e568be2c1d911d12881532746e11602051b
akmaldira <akmaldiraa@gmail.com>
2022-03-15 00:31:27 +0700

    add auth endpoint

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const { verifyToken } = require('../middleware/verifyToken');
 router.post('/signup', signUpHandler);
 router.post('/signin', signInHandler);
 router.delete('/logout', logoutHandler);
-
+router.get('/dashboard', verifyToken);
 router.get('/auth', refreshToken);
 
 module.exports = router;
